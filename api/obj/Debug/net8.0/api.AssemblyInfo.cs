0c6cbf10c97a49c4a415ed001695793461b08f55
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-02-26 11:54:52 +0500

    feat : JWT Token Service

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02a3a589b8fc9c8cbe410195bfc162d2a3e8669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793ab9a5467649aa981e09a43e0f7758f97b01a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
